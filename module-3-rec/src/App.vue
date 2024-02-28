9788e5c545c453d32bb7eb74357db0fe711534bf
Md.SurujMiah <77936133+smshaown@users.noreply.github.com>
2024-02-28 22:35:39 +0600

    added the code

---
+++ module-3-rec/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { images } from "./data/data.js";
+</script>
+
+<template>
+  <div class="">
+    <!-- <h1>{{ data.message }}</h1>
+    <p class="mt-3">{{ data.date }}</p>
+    <h5>
+      You have <span>{{ data.task[4] }}</span>
+    </h5>
+    <h6>{{ data.name }}</h6> -->
+  </div>
+
+  <section>
+    <div class="row">
+      <div class="col">
+        <img src="{{ images[0].image }}" alt="" />
+        <h3>{{ images[1].title }}</h3>
+        <button>button</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ module-3-rec/src/data/data.js
@@ -0,0 +1,29 @@
+
+const data = {
+    message: "welcome. get ready to master vue .js",
+    date: "30th june, 20323",
+    task: [1, 2, "no", 4, 5],
+    name: "s m shaown"
+  };
+
+
+  const images = [
+    {
+      image: "https://www.w3schools.com/css/img_5terre.jpg",
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, aperiam.",
+      link : "https://www.w3schools.com/css/css_image_gallery.asp",
+    },
+    {
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, aperiam.",
+        link : "https://www.w3schools.com/css/css_image_gallery.asp",
+        image: "https://www.w3schools.com/css/img_forest.jpg",
+    }, 
+    {
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, aperiam.",
+        link : "https://www.w3schools.com/css/css_image_gallery.asp",
+        image: "https://www.w3schools.com/css/img_mountains.jpg",
+    }
+  ]
+
+//   export { data };
+export default images;
